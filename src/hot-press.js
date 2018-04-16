0f3f6a07323f843526978d6e96cae6e218cfadf9
John Wright <johngeorge.wright@gmail.com>
2018-04-16 15:39:49 +0100

    refactor: make hierarchy testable

--- src/hot-press.js
+++ src/hot-press.js
@@ -1,13 +1,7 @@
 import {errorAfterMS, findDuplicates, flatten, upperFirst} from './util'
 import HotPressExistingProcedureError from './hot-press-existing-procedure-error'
 import HotPressNonExistingProcedureError from './hot-press-non-existing-procedure-error'
-
-/**
- * String representing wildcards
- * @private
- * @type {String}
- */
-const WILDCARD = '*'
+import {HIERARCHY_SEPARATOR, getHierarchy, prependHierarchy, removePrefix} from './hierarchy'
 
 /**
  * The error namespace
@@ -24,13 +18,6 @@ const ERROR = 'error'
 const ON = 'on'
 
 /**
- * What separates namespace hierarchy
- * @private
- * @type {String}
- */
-const HIERARCHY_SEPARATOR = '.'
-
-/**
  * The default lifecycle.
  * @private
  * @type {String[]}
@@ -43,50 +30,6 @@ const DEFAULT_LIFECYCLE = ['before', ON, 'after']
  * @type {Number}
  */
 const DEFAULT_TIMEOUT = 300
-
-/**
- * Returns a list of messages that should be called in order for a specific
- * message.
- * @private
- * @example
- * getHierarchy('a.b.c.d')
- * // ==> ['a.b.c.d', 'a.b.c.*', 'a.b.*', 'a.*', '*']
- * @param {String} message - The event name/message
- * @return {String[]} The ordered list of messages that should be called
- */
-const getHierarchy = message => {
-  const parts = message.split(HIERARCHY_SEPARATOR)
-  const hierarchy = [WILDCARD]
-  parts.reduce((message, part) => {
-    const prefix = message + HIERARCHY_SEPARATOR
-    hierarchy.unshift(prefix + WILDCARD)
-    return prefix + part
-  })
-  hierarchy.unshift(message)
-  return hierarchy
-}
-
-/**
- * Prepends an event name, if the string exists.
- * @private
- * @param {String} name - The event name/message
- * @param {String} prefix - The prefix to prepend
- * @return {String} The transformed string
- */
-const prependHierarchy = (name, prefix) =>
-  prefix ? `${prefix}.${name}` : name
-
-/**
- * Removes a given namespace prefixed on a string.
- * @private
- * @param  {String} name   The event name/message
- * @param  {String} prefix The prefix to remove
- * @return {String}        The transformed string
- */
-const removePrefix = (name, prefix) =>
-  prefix
-    ? name.substr(prefix.length + HIERARCHY_SEPARATOR.length)
-    : name
 
 /**
  * Returns a method name we may use to attach a single listener.
